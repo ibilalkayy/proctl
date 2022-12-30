df30b39efdfc20a2609f9a81849d7e231350d173
Bilal Khan <bilalkhanrecovered@gmail.com>
2022-12-30 10:30:16 +0500

    Generate the JWT and set credentials and token

---
+++ database/redis/set.go
@@ -0,0 +1,48 @@
+package redis
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/go-redis/redis"
+)
+
+type NewToken struct {
+	MyToken string
+}
+
+func RedisConnect() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	return client
+}
+
+func SetCredentials(email, password string) {
+	client := RedisConnect()
+	insertEmails, err := client.LPush("Emails", email).Result()
+	if err != nil {
+		fmt.Println(insertEmails)
+	}
+
+	insertPasswords, err := client.LPush("Passwords", password).Result()
+	if err != nil {
+		fmt.Println(insertPasswords)
+	}
+}
+
+func SetToken(id, tokenString string) {
+	client := RedisConnect()
+	json, err := json.Marshal(NewToken{MyToken: tokenString})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := client.Set(id, json, 0).Err(); err != nil {
+		log.Fatal(err)
+	}
+}
