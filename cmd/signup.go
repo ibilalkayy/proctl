89ea256900a2a9b170e8071ef68f8c9ff14c7ff4
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-01-09 10:02:34 +0500

    Started working on the verification functionality

--- cmd/signup.go
+++ cmd/signup.go
@@ -9,9 +9,34 @@ import (
 	"github.com/ibilalkayy/proctl/database/mysql"
 	"github.com/ibilalkayy/proctl/database/redis"
 	"github.com/ibilalkayy/proctl/jwt"
+	"github.com/ibilalkayy/proctl/middleware"
 	"github.com/spf13/cobra"
 	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/gomail.v2"
 )
+
+func Verify(toEmail, accountName string) {
+	mail := gomail.NewMessage()
+	myEmail := middleware.LoadEnvVariable("APP_EMAIL")
+	myPassword := middleware.LoadEnvVariable("APP_PASSWORD")
+	mail.SetHeader("From", myEmail)
+	mail.SetHeader("To", toEmail)
+	mail.SetHeader("Reply-To", myEmail)
+	mail.SetHeader("Subject", "[proctl] Confirm your email address")
+	mail.SetBody("text/html", fmt.Sprintf(`
+	<center><h1>We're glad you're here, %s.</h1></center>
+	<center>We just want to confirm it's you.<br><br></center>
+	<center>
+		<button style="background-color:#008CBA; border-color:#008CBA; border-radius: 4px; color:white; height: 50px; width: 300px;">
+			Click to confirm your email address
+		</button>
+	<center>
+	<center><br>If you didn't create a proctl account, just delete this email.</center>`, accountName))
+	a := gomail.NewDialer("smtp.gmail.com", 587, myEmail, myPassword)
+	if err := a.DialAndSend(mail); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func emailValid(email string) bool {
 	regexEmail := regexp.MustCompile(`^[a-zA-Z0-9-_]+@[a-z]+\.[a-z]{1,3}$`)
@@ -49,6 +74,8 @@ var signupCmd = &cobra.Command{
 					_, _, redisSignupAccountName, _ := redis.GetCredentials()
 					redis.SetAccountInfo("LoginToken", tokenString)
 					redis.SetAccountInfo("AccountName", redisSignupAccountName[0])
+					accountName := redis.GetAccountInfo("AccountName")
+					Verify(signupEmail, accountName)
 					fmt.Println("You have successfully created an account.")
 				} else {
 					fmt.Println("Signup failure.")
--- go.mod
+++ go.mod
@@ -3,21 +3,24 @@ module github.com/ibilalkayy/proctl
 go 1.19
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/go-sql-driver/mysql v1.7.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.6.1
 	github.com/spf13/viper v1.14.0
 	golang.org/x/crypto v0.4.0
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.24.2 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/spf13/afero v1.9.2 // indirect
@@ -27,6 +30,7 @@ require (
 	github.com/subosito/gotenv v1.4.1 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
