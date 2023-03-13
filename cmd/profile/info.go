86f9205bcef5457e3f7d0a95dd07c09b72e57e57
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-03-13 09:42:31 +0500

    added the info file

---
+++ cmd/profile/info.go
@@ -0,0 +1,33 @@
+package profile
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/ibilalkayy/proctl/cmd"
+	"github.com/ibilalkayy/proctl/database/mysql"
+	"github.com/ibilalkayy/proctl/database/redis"
+	"github.com/ibilalkayy/proctl/jwt"
+	"github.com/spf13/cobra"
+)
+
+// infoCmd represents the info command
+var infoCmd = &cobra.Command{
+	Use:   "info",
+	Short: "Show the profile information",
+	Run: func(cmd *cobra.Command, args []string) {
+		loginToken := redis.GetAccountInfo("LoginToken")
+		accountEmail := redis.GetAccountInfo("AccountEmail")
+		accountPassword := redis.GetAccountInfo("AccountPassword")
+		if len(loginToken) != 0 && jwt.RefreshToken() {
+			mysql.ListUserInfo(accountEmail, accountPassword)
+			mysql.ListProfileInfo(accountEmail)
+		} else {
+			fmt.Println(errors.New("First login to show the profile information."))
+		}
+	},
+}
+
+func init() {
+	cmd.RootCmd.AddCommand(infoCmd)
+}
