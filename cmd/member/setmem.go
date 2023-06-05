a52d7c207c9896b4e3b9173dcadb856a1afd0967
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 09:17:24 +0500

    added comments on setmem.go

--- cmd/member/setmem.go
+++ cmd/member/setmem.go
@@ -1,5 +1,7 @@
+// Package member contains functionality related to managing members.
 package member
 
+// Import necessary packages and libraries.
 import (
 	"errors"
 	"fmt"
@@ -13,23 +15,32 @@ import (
 )
 
 // setmemCmd represents the setmem command
+// This is the command that will be used to setup member credentials.
 var setmemCmd = &cobra.Command{
-	Use:   "setmem",
-	Short: "Setup the member credentials",
+	Use:   "setmem",                       // This is the command name.
+	Short: "Setup the member credentials", // A short description of the command.
+	// This function is run when the command is called.
 	Run: func(cmd *cobra.Command, args []string) {
+		// Get the values of the flags (email, password, full name, account name).
 		memberEmail, _ := cmd.Flags().GetString("email")
 		memberPassword, _ := cmd.Flags().GetString("password")
 		memberFullName, _ := cmd.Flags().GetString("full name")
 		memberAccountName, _ := cmd.Flags().GetString("account name")
 
+		// Get login token from Redis.
 		loginToken := redis.GetAccountInfo("LoginToken")
 		memberLoginToken := redis.GetAccountInfo("MemberLoginToken")
 		memberCred, memberFound := mysql.FindMember(memberEmail, "")
 		hashPass := middleware.HashPassword([]byte(memberPassword))
+
+		// Create member credentials array.
 		memberCredentials := [3]string{memberEmail, hashPass, memberAccountName}
 
+		// If the user is not logged in, generate a JWT, store credentials in Redis, and update or insert the member in MySQL.
 		if len(loginToken) == 0 && len(memberLoginToken) == 0 {
 			tokenString, jwtTokenGenerated := jwt.GenerateJWT()
+
+			// Check if all required fields are present and the member is not yet set.
 			if len(memberEmail) != 0 && len(memberPassword) != 0 && len(memberFullName) != 0 && len(memberAccountName) != 0 {
 				if len(memberCred[1]) == 0 && len(memberCred[2]) == 0 && len(memberCred[3]) == 0 {
 					if jwtTokenGenerated {
@@ -62,8 +73,12 @@ var setmemCmd = &cobra.Command{
 	},
 }
 
+// init is a special Go function that is automatically executed at the start of the program.
 func init() {
+	// This function adds setmemCmd to the list of commands in the root command.
 	cmd.RootCmd.AddCommand(setmemCmd)
+
+	// This function adds flags to setmemCmd for specifying member credentials.
 	setmemCmd.Flags().StringP("email", "e", "", "Specify an email address to setup the credentials")
 	setmemCmd.Flags().StringP("password", "p", "", "Specify a password to setup the credentials")
 	setmemCmd.Flags().StringP("full name", "f", "", "Specify a full name to setup the credentials")
