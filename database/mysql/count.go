e5cc363e24303e37db555263ad737b05d028f7c2
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 11:17:58 +0500

    added comments on count.go

--- database/mysql/count.go
+++ database/mysql/count.go
@@ -2,12 +2,18 @@ package mysql
 
 import "log"
 
+// CountTableColumns counts the number of columns in a table in the MySQL database
 func CountTableColumns(table string) int {
+	// Connect to the MySQL database
 	db := Connect()
+
 	var counted int
 	q := "SELECT COUNT(*) FROM " + table
+
+	// Execute the query and retrieve the count
 	if err := db.QueryRow(q).Scan(&counted); err != nil {
 		log.Fatal(err)
 	}
+
 	return counted
 }
