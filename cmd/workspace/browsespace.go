2eeb8ffe318ecdb396b858d3874aacdf667d691c
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 10:59:12 +0500

    added comments on browsespace.go

--- cmd/workspace/browsespace.go
+++ cmd/workspace/browsespace.go
@@ -16,19 +16,22 @@ var browsespaceCmd = &cobra.Command{
 	Use:   "browsespace",
 	Short: "Browse all the workspaces",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Get the login token, account email, member login token, and member email from Redis
 		loginToken := redis.GetAccountInfo("LoginToken")
 		accountEmail := redis.GetAccountInfo("AccountEmail")
 		memberLoginToken := redis.GetAccountInfo("MemberLoginToken")
 		memberEmail := redis.GetAccountInfo("MemberEmail")
 
 		if len(loginToken) != 0 && jwt.RefreshToken("user") {
+			// Retrieve and print the workspace name for the user
 			name := mysql.FindWorkspace(accountEmail)
 			fmt.Println(name)
 		} else if len(memberLoginToken) != 0 && jwt.RefreshToken("member") {
+			// Retrieve and print the workspace name for the member
 			name := mysql.FindWorkspace(memberEmail)
 			fmt.Println(name)
 		} else {
-			fmt.Println(errors.New("First login to add a new workspace"))
+			fmt.Println(errors.New("First login to browse workspaces"))
 		}
 	},
 }
