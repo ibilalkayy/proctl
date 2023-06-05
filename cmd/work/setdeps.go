d1ebf3eafab4efb726a469075c00ea54be9f58dd
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 10:41:39 +0500

    added comments on setdeps.go

--- cmd/work/setdeps.go
+++ cmd/work/setdeps.go
@@ -16,18 +16,23 @@ var setdepsCmd = &cobra.Command{
 	Use:   "setdeps",
 	Short: "Setup the department of a user",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Get the department flag value from the command
 		department, _ := cmd.Flags().GetString("department")
 
+		// Get the user's account email and login token from Redis
 		accountEmail := redis.GetAccountInfo("AccountEmail")
 		loginToken := redis.GetAccountInfo("LoginToken")
 
+		// Get the member's account email and login token from Redis
 		memberAccountEmail := redis.GetAccountInfo("MemberAccountEmail")
 		memberLoginToken := redis.GetAccountInfo("MemberLoginToken")
 
+		// Find the department information for the user and member
 		userCredentials := mysql.FindDepartment(accountEmail)
 		memberCredentials := mysql.FindDepartment(memberAccountEmail)
 
 		if len(loginToken) != 0 && jwt.RefreshToken("user") {
+			// Check if the user has not already inserted a department
 			if len(userCredentials[0]) == 0 && len(userCredentials[1]) == 0 {
 				redis.DelToken("MemberLoginToken")
 				mysql.InsertDepartment(accountEmail, department)
@@ -37,6 +42,7 @@ var setdepsCmd = &cobra.Command{
 				fmt.Println(errors.New("You have already inserted the department. Please update it"))
 			}
 		} else if len(memberLoginToken) != 0 && jwt.RefreshToken("member") {
+			// Check if the member has not already inserted a department
 			if len(memberCredentials[0]) == 0 && len(memberCredentials[1]) == 0 {
 				redis.DelToken("LoginToken")
 				mysql.InsertDepartment(memberAccountEmail, department)
