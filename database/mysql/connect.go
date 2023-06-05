e4166a5b81ce1e3d24b65f914694a8e30360e694
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 11:15:40 +0500

    added comments on connect.go

--- database/mysql/connect.go
+++ database/mysql/connect.go
@@ -12,13 +12,19 @@ import (
 	"github.com/ibilalkayy/proctl/middleware"
 )
 
+// Connect establishes a connection to the MySQL database
 func Connect() (db *sql.DB) {
-	db_user := middleware.LoadEnvVariable("DB_USER")
-	db_password := middleware.LoadEnvVariable("DB_PASSWORD")
-	db_address := middleware.LoadEnvVariable("DB_ADDRESS")
-	db_db := middleware.LoadEnvVariable("DB_DB")
-	s := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", db_user, db_password, db_address, db_db)
-	db, err := sql.Open("mysql", s)
+	// Load database connection details from environment variables
+	dbUser := middleware.LoadEnvVariable("DB_USER")
+	dbPassword := middleware.LoadEnvVariable("DB_PASSWORD")
+	dbAddress := middleware.LoadEnvVariable("DB_ADDRESS")
+	dbDB := middleware.LoadEnvVariable("DB_DB")
+
+	// Create the connection string
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", dbUser, dbPassword, dbAddress, dbDB)
+
+	// Open a connection to the MySQL database
+	db, err := sql.Open("mysql", connectionString)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,15 +32,21 @@ func Connect() (db *sql.DB) {
 	return db
 }
 
+// CreateTable creates a table in the MySQL database using SQL queries from a file
 func CreateTable(number int) (db *sql.DB) {
+	// Connect to the MySQL database
 	db = Connect()
+
+	// Read the SQL queries from the file
 	query, err := ioutil.ReadFile("database/mysql/db.SQL")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Split the queries into separate requests
 	requests := strings.Split(string(query), ";")[number]
 
+	// Prepare and execute each request
 	stmt, err := db.Prepare(requests)
 	if err != nil {
 		log.Fatal(err)
