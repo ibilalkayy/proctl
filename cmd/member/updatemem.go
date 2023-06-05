f94b04008b2e91399f041d3e485b079667a13b22
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 09:30:02 +0500

    added comments on updatemem.go

--- cmd/member/updatemem.go
+++ cmd/member/updatemem.go
@@ -12,9 +12,11 @@ import (
 )
 
 // updatememCmd represents the updatemem command
+// This is the command that will be used to update member credentials.
 var updatememCmd = &cobra.Command{
-	Use:   "updatemem",
-	Short: "Update the member credentials",
+	Use:   "updatemem",                     // This is the command name.
+	Short: "Update the member credentials", // A short description of the command.
+	// This function is run when the command is called.
 	Run: func(cmd *cobra.Command, args []string) {
 		memberEmail, _ := cmd.Flags().GetString("email")
 		memberPassword, _ := cmd.Flags().GetString("password")
@@ -25,12 +27,15 @@ var updatememCmd = &cobra.Command{
 		memberLocation, _ := cmd.Flags().GetString("location")
 		memberWorkingStatus, _ := cmd.Flags().GetString("working status")
 
+		// Get login tokens and account info from Redis, and find the member in MySQL.
 		loginToken := redis.GetAccountInfo("LoginToken")
 		memberLoginToken := redis.GetAccountInfo("MemberLoginToken")
 		memberAccountEmail := redis.GetAccountInfo("MemberAccountEmail")
 		memberAccountPassword := redis.GetAccountInfo("MemberAccountPassword")
 		_, memberFound := mysql.FindMember(memberAccountEmail, memberAccountPassword)
 
+		// Check login status and refresh JWT if necessary.
+		// If logged in as a user, show an error. If logged in as a member, update member credentials.
 		if len(loginToken) != 0 && jwt.RefreshToken("user") && memberFound {
 			fmt.Println(errors.New("Can't update the member credentials as an admin user"))
 		} else if len(memberLoginToken) != 0 && jwt.RefreshToken("member") && memberFound {
@@ -43,7 +48,9 @@ var updatememCmd = &cobra.Command{
 	},
 }
 
+// init is a special Go function that is automatically executed at the start of the program.
 func init() {
+	// This function adds updatememCmd to the list of commands in the root command.
 	cmd.RootCmd.AddCommand(updatememCmd)
 	updatememCmd.Flags().StringP("email", "e", "", "Specify an email address to update")
 	updatememCmd.Flags().StringP("password", "p", "", "Specify a password to update")
