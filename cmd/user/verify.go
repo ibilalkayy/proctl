42147feec1e10c6464a8625fd5a549da3f0dcd28
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 10:34:11 +0500

    added comments on verify.go

--- cmd/user/verify.go
+++ cmd/user/verify.go
@@ -17,6 +17,7 @@ var verifyCmd = &cobra.Command{
 	Use:   "verify",
 	Short: "Check verification of a user",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Check if a login token exists for the user
 		loginToken := redis.GetAccountInfo("LoginToken")
 		if len(loginToken) != 0 && jwt.RefreshToken("user") {
 			AccountName := redis.GetAccountInfo("AccountName")
@@ -25,23 +26,28 @@ var verifyCmd = &cobra.Command{
 			AccountPassword := redis.GetAccountInfo("AccountPassword")
 			getVerificationCode := redis.GetAccountInfo("VerificationCode")
 
+			// Check if the account is not already verified and has a verification code
 			_, _, mysqlStatus, _ := mysql.FindAccount(AccountEmail, AccountPassword)
 			if mysqlStatus == "0" && len(getVerificationCode) != 0 {
 
+				// Send verification email
 				accountEmail := redis.GetAccountInfo("AccountEmail")
 				accountPassword := redis.GetAccountInfo("AccountPassword")
-
 				accountCode := GetRandomCode(accountEmail, accountPassword)
 				values := [5]string{"account-template", AccountName, accountCode, AccountEmail, "[proctl] Confirm your email address"}
 				email.Verify(values)
+
 				var verificationCode string
 				fmt.Printf("Enter the verification code: ")
 				fmt.Scanln(&verificationCode)
 
+				// Delete the verification code and update the user status in the database
 				if getVerificationCode == verificationCode {
 					redis.DelToken("VerificationCode")
 					userData := [4]string{AccountFullName, AccountName, AccountPassword, "1"}
 					mysql.UpdateUser(userData, AccountEmail, AccountPassword)
+
+					// Generate a new login token
 					tokenString, _ := jwt.GenerateJWT()
 					redis.SetAccountInfo("LoginToken", tokenString)
 					fmt.Println("You're successfully verified")
