22b0bfe719c62f035ba854f8350ab7d552df5c98
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 10:56:47 +0500

    added comments on updateroles.go

--- cmd/work/updateroles.go
+++ cmd/work/updateroles.go
@@ -14,20 +14,25 @@ import (
 // updaterolesCmd represents the updateroles command
 var updaterolesCmd = &cobra.Command{
 	Use:   "updateroles",
-	Short: "A brief description of your command",
+	Short: "Update the role of a user",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Get the role flag value from the command
 		role, _ := cmd.Flags().GetString("role")
 
+		// Get the user's account email and login token from Redis
 		accountEmail := redis.GetAccountInfo("AccountEmail")
 		loginToken := redis.GetAccountInfo("LoginToken")
 
+		// Get the member's account email and login token from Redis
 		memberAccountEmail := redis.GetAccountInfo("MemberAccountEmail")
 		memberLoginToken := redis.GetAccountInfo("MemberLoginToken")
 
+		// Find the role information for the user and member
 		userCredentials := mysql.FindRole(accountEmail)
 		memberCredentials := mysql.FindRole(memberAccountEmail)
 
 		if len(loginToken) != 0 && jwt.RefreshToken("user") || len(memberLoginToken) != 0 && jwt.RefreshToken("member") {
+			// Check if the user has already inserted a role
 			if len(userCredentials[0]) != 0 && len(userCredentials[1]) != 0 {
 				mysql.UpdateRole(accountEmail, role)
 				fmt.Println("Your have successfully updated the role")
@@ -37,7 +42,7 @@ var updaterolesCmd = &cobra.Command{
 				fmt.Println("Your have successfully updated the role")
 				return
 			} else {
-				fmt.Println(errors.New("You have not inserted the role. How you can update it?"))
+				fmt.Println(errors.New("You have not inserted the role. How can you update it?"))
 			}
 		} else {
 			fmt.Println(errors.New("First login to update the role"))
