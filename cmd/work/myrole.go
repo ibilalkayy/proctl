75d7d13a52842bef0265bda03df426f88eb90286
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 10:39:27 +0500

    added comments on myrole.go

--- cmd/work/myrole.go
+++ cmd/work/myrole.go
@@ -16,16 +16,20 @@ var myroleCmd = &cobra.Command{
 	Use:   "myrole",
 	Short: "Show you the role",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Get the user's account email and login token from Redis
 		accountEmail := redis.GetAccountInfo("AccountEmail")
 		loginToken := redis.GetAccountInfo("LoginToken")
 
+		// Get the member's account email and login token from Redis
 		memberAccountEmail := redis.GetAccountInfo("MemberAccountEmail")
 		memberLoginToken := redis.GetAccountInfo("MemberLoginToken")
 
+		// Find the role information for the user and member
 		userCredentials := mysql.FindRole(accountEmail)
 		memberCredentials := mysql.FindRole(memberAccountEmail)
 
 		if len(loginToken) != 0 && jwt.RefreshToken("user") {
+			// Check if the user has role credentials
 			if len(userCredentials[0]) != 0 && len(userCredentials[1]) != 0 {
 				fmt.Printf("My role: %s\n", userCredentials[1])
 				return
@@ -33,14 +37,15 @@ var myroleCmd = &cobra.Command{
 				fmt.Println(errors.New("This user has no role"))
 			}
 		} else if len(memberLoginToken) != 0 && jwt.RefreshToken("member") {
+			// Check if the member has role credentials
 			if len(memberCredentials[0]) != 0 && len(memberCredentials[1]) != 0 {
 				fmt.Printf("My role: %s\n", memberCredentials[1])
 				return
 			} else {
-				fmt.Println(errors.New("not a member role"))
+				fmt.Println(errors.New("This member has no role"))
 			}
 		} else {
-			fmt.Println(errors.New("This member has no role"))
+			fmt.Println(errors.New("First login to show you the role"))
 		}
 	},
 }
