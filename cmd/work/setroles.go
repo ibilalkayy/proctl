de2d6c7fae0a6cb98ea4d9b19057f50b2501533e
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 10:42:51 +0500

    added comments on setroles.go

--- cmd/work/setroles.go
+++ cmd/work/setroles.go
@@ -16,18 +16,23 @@ var setrolesCmd = &cobra.Command{
 	Use:   "setroles",
 	Short: "Setup the role of a user",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Get the role flag value from the command
 		role, _ := cmd.Flags().GetString("role")
 
+		// Get the user's account email and login token from Redis
 		accountEmail := redis.GetAccountInfo("AccountEmail")
 		loginToken := redis.GetAccountInfo("LoginToken")
 
+		// Get the member's account email and login token from Redis
 		memberAccountEmail := redis.GetAccountInfo("MemberAccountEmail")
 		memberLoginToken := redis.GetAccountInfo("MemberLoginToken")
 
+		// Find the role information for the user and member
 		userCredentials := mysql.FindRole(accountEmail)
 		memberCredentials := mysql.FindRole(memberAccountEmail)
 
 		if len(loginToken) != 0 && jwt.RefreshToken("user") {
+			// Check if the user has not already inserted a role
 			if len(userCredentials[0]) == 0 && len(userCredentials[1]) == 0 {
 				redis.DelToken("MemberLoginToken")
 				mysql.InsertRole(accountEmail, role)
@@ -37,6 +42,7 @@ var setrolesCmd = &cobra.Command{
 				fmt.Println(errors.New("You have already inserted the role. Please update it"))
 			}
 		} else if len(memberLoginToken) != 0 && jwt.RefreshToken("member") {
+			// Check if the member has not already inserted a role
 			if len(memberCredentials[0]) == 0 && len(memberCredentials[1]) == 0 {
 				redis.DelToken("LoginToken")
 				mysql.InsertRole(memberAccountEmail, role)
