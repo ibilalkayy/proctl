15c53c184e7b0df99b5c6cc2bba15b9699de5e9d
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 10:48:11 +0500

    added comments on showdeps.go

--- cmd/work/showdeps.go
+++ cmd/work/showdeps.go
@@ -13,14 +13,33 @@ import (
 // showdepsCmd represents the showdeps command
 var showdepsCmd = &cobra.Command{
 	Use:   "showdeps",
-	Short: "Show the deparments of a member and the admin",
+	Short: "Show the departments of a member and the admin",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Get the login tokens from Redis
 		loginToken := redis.GetAccountInfo("LoginToken")
 		memberLoginToken := redis.GetAccountInfo("MemberLoginToken")
-		if (len(loginToken) != 0 && jwt.RefreshToken("user")) || (len(memberLoginToken) != 0 && jwt.RefreshToken("member")) {
-			roles := [12]string{" Sales & CRM", " Legal", " HR & Recruiting", " Marketing", " Software Development", " Finance", " Education", " Operations", " Product Management", "Construction", "Nonprofits", "IT"}
-			fmt.Println("Following are the departments")
 
+		// Check if the user or member is logged in and has valid login tokens
+		if (len(loginToken) != 0 && jwt.RefreshToken("user")) || (len(memberLoginToken) != 0 && jwt.RefreshToken("member")) {
+			// Define the available departments
+			roles := [12]string{
+				"Sales & CRM",
+				"Legal",
+				"HR & Recruiting",
+				"Marketing",
+				"Software Development",
+				"Finance",
+				"Education",
+				"Operations",
+				"Product Management",
+				"Construction",
+				"Nonprofits",
+				"IT",
+			}
+
+			fmt.Println("Following are the departments:")
+
+			// Print the departments with index numbers
 			index := 1
 			for _, value := range roles {
 				fmt.Printf("%d. %s\n", index, value)
