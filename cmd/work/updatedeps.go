dccb874024d995b8ae30759573ba452bf2c24d82
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 10:54:48 +0500

    added comments on updatedeps.go

--- cmd/work/updatedeps.go
+++ cmd/work/updatedeps.go
@@ -14,20 +14,25 @@ import (
 // updatedepsCmd represents the updatedeps command
 var updatedepsCmd = &cobra.Command{
 	Use:   "updatedeps",
-	Short: "A brief description of your command",
+	Short: "Update the department of a user",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Get the department flag value from the command
 		department, _ := cmd.Flags().GetString("department")
 
+		// Get the user's account email and login token from Redis
 		accountEmail := redis.GetAccountInfo("AccountEmail")
 		loginToken := redis.GetAccountInfo("LoginToken")
 
+		// Get the member's account email and login token from Redis
 		memberAccountEmail := redis.GetAccountInfo("MemberAccountEmail")
 		memberLoginToken := redis.GetAccountInfo("MemberLoginToken")
 
+		// Find the department information for the user and member
 		userCredentials := mysql.FindDepartment(accountEmail)
 		memberCredentials := mysql.FindDepartment(memberAccountEmail)
 
 		if len(loginToken) != 0 && jwt.RefreshToken("user") || len(memberLoginToken) != 0 && jwt.RefreshToken("member") {
+			// Check if the user has already inserted a department
 			if len(userCredentials[0]) != 0 && len(userCredentials[1]) != 0 {
 				mysql.UpdateDepartment(accountEmail, department)
 				fmt.Println("Your have successfully updated the department")
@@ -37,7 +42,7 @@ var updatedepsCmd = &cobra.Command{
 				fmt.Println("Your have successfully updated the department")
 				return
 			} else {
-				fmt.Println(errors.New("You have not inserted the department. How you can update it?"))
+				fmt.Println(errors.New("You have not inserted the department. How can you update it?"))
 			}
 		} else {
 			fmt.Println(errors.New("First login to update the department"))
