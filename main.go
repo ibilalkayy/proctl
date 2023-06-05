d6fed0eefaf18a3ca4d9ededa350e21e87b27b21
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 08:41:00 +0500

    added comments in the main.go

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
+// Package main is the entry point of the program.
 package main
 
+// Import necessary packages and libraries.
 import (
+	// This imports the cmd package from the github.com/ibilalkayy/proctl repository.
 	"github.com/ibilalkayy/proctl/cmd"
+
+	// The following import statements import specific commands from the proctl tool.
+	// The "_" before the package means that the package is being imported for its side-effects only
+	// i.e., it's only being imported to execute an initialization function.
 	_ "github.com/ibilalkayy/proctl/cmd/board"
 	_ "github.com/ibilalkayy/proctl/cmd/member"
 	_ "github.com/ibilalkayy/proctl/cmd/profile"
@@ -10,6 +17,9 @@ import (
 	_ "github.com/ibilalkayy/proctl/cmd/workspace"
 )
 
+// Function main is the entry point of the program.
 func main() {
+	// Call the Execute function from the cmd package.
+	// This will start the execution of the program.
 	cmd.Execute()
 }
