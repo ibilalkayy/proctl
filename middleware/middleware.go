fff3300378017373de7b96f298db9116b9f517cf
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 11:48:15 +0500

    added comments on middleware.go

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -7,6 +7,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// LoadEnvVariable loads an environment variable value from the .env file
 func LoadEnvVariable(key string) string {
 	viper.SetConfigFile(".env")
 	err := viper.ReadInConfig()
@@ -22,12 +23,14 @@ func LoadEnvVariable(key string) string {
 	return value
 }
 
+// HandleError logs and terminates the program if an error occurs
 func HandleError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+// HashPassword hashes the given password using bcrypt
 func HashPassword(value []byte) string {
 	hash, err := bcrypt.GenerateFromPassword(value, bcrypt.MinCost)
 	if err != nil {
