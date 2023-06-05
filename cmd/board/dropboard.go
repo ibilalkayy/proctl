c3af9219bc28a45db4373348501b840afb2b63a8
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 08:46:46 +0500

    added comments on dropboard

--- cmd/board/dropboard.go
+++ cmd/board/dropboard.go
@@ -1,5 +1,7 @@
+// Package board contains functionality related to managing boards.
 package board
 
+// Import necessary packages and libraries.
 import (
 	"errors"
 	"fmt"
@@ -11,22 +13,30 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// dropboardCmd represents the dropboard command
+// dropboardCmd represents the dropboard command.
+// This is the command that will be used to delete a board.
 var dropboardCmd = &cobra.Command{
-	Use:   "dropboard",
-	Short: "Delete the board",
+	Use:   "dropboard",        // This is the command name.
+	Short: "Delete the board", // A short description of the command.
+	// This function is run when the command is called.
 	Run: func(cmd *cobra.Command, args []string) {
+		// Get board name flag
 		boardName, _ := cmd.Flags().GetString("board")
 
+		// Get account information from Redis.
 		accountEmail := redis.GetAccountInfo("AccountEmail")
 		loginToken := redis.GetAccountInfo("LoginToken")
 
+		// Get member account information from Redis.
 		memberAccountEmail := redis.GetAccountInfo("MemberAccountEmail")
 		memberLoginToken := redis.GetAccountInfo("MemberLoginToken")
 
+		// Get board details from MySQL.
 		userBoard := mysql.FindBoard(accountEmail, boardName)
 		memberBoard := mysql.FindBoard(memberAccountEmail, boardName)
 
+		// Check if user is logged in and refresh the token.
+		// If user or member is logged in and their board exists, delete it. Otherwise, output an error.
 		if len(loginToken) != 0 && jwt.RefreshToken("user") || len(memberLoginToken) != 0 && jwt.RefreshToken("member") {
 			if len(userBoard) != 0 {
 				mysql.DeleteBoard(accountEmail, boardName)
@@ -43,7 +53,11 @@ var dropboardCmd = &cobra.Command{
 	},
 }
 
+// init is a special Go function that is automatically executed at the start of the program.
 func init() {
+	// This function adds dropboardCmd to the list of commands in the root command.
 	cmd.RootCmd.AddCommand(dropboardCmd)
+
+	// This function adds a flag to dropboardCmd for specifying the board name.
 	dropboardCmd.Flags().StringP("board", "b", "", "Specify the baord name to delete")
 }
