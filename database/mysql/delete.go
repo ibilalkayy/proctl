b77625dd6e607c017bc160566ab6da547d4795fa
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 11:20:47 +0500

    added comments on delete.go

--- database/mysql/delete.go
+++ database/mysql/delete.go
@@ -8,33 +8,33 @@ import (
 )
 
 func DeleteWorkspace(value [2]string) {
-	db := Connect()
-	q := "DELETE FROM Workspaces WHERE emails=? AND names=?"
-	delete, err := db.Prepare(q)
-	middleware.HandleError(err)
+	db := Connect()                                          // Connect to the MySQL database
+	q := "DELETE FROM Workspaces WHERE emails=? AND names=?" // SQL query to delete a workspace
+	delete, err := db.Prepare(q)                             // Prepare the SQL statement
+	middleware.HandleError(err)                              // Handle any errors that occur
 
-	defer delete.Close()
+	defer delete.Close() // Close the prepared statement after the function ends
 
 	if len(value[0]) != 0 && len(value[1]) != 0 {
-		_, err = delete.Exec(value[0], value[1])
-		middleware.HandleError(err)
+		_, err = delete.Exec(value[0], value[1]) // Execute the prepared statement with the provided values
+		middleware.HandleError(err)              // Handle any errors that occur during execution
 	} else {
-		fmt.Println(errors.New("Flags are required to delete the workspace"))
+		fmt.Println(errors.New("Flags are required to delete the workspace")) // Print an error message if required values are missing
 	}
 }
 
 func DeleteBoard(email, board string) {
-	db := Connect()
-	q := "DELETE FROM Boards WHERE emails=? AND boards=?"
-	delete, err := db.Prepare(q)
-	middleware.HandleError(err)
+	db := Connect()                                       // Connect to the MySQL database
+	q := "DELETE FROM Boards WHERE emails=? AND boards=?" // SQL query to delete a board
+	delete, err := db.Prepare(q)                          // Prepare the SQL statement
+	middleware.HandleError(err)                           // Handle any errors that occur
 
-	defer delete.Close()
+	defer delete.Close() // Close the prepared statement after the function ends
 
 	if len(email) != 0 && len(board) != 0 {
-		_, err = delete.Exec(email, board)
-		middleware.HandleError(err)
+		_, err = delete.Exec(email, board) // Execute the prepared statement with the provided values
+		middleware.HandleError(err)        // Handle any errors that occur during execution
 	} else {
-		fmt.Println(errors.New("Flags are required to delete the board"))
+		fmt.Println(errors.New("Flags are required to delete the board")) // Print an error message if required values are missing
 	}
 }
