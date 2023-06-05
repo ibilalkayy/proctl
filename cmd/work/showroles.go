b64a82ace1b37aaadfcc68f8763930f5e2f9cf50
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-05 10:50:04 +0500

    added comments on showroles.go

--- cmd/work/showroles.go
+++ cmd/work/showroles.go
@@ -15,12 +15,23 @@ var showrolesCmd = &cobra.Command{
 	Use:   "showroles",
 	Short: "Show the roles of a member and the admin",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Get the login tokens from Redis
 		loginToken := redis.GetAccountInfo("LoginToken")
 		memberLoginToken := redis.GetAccountInfo("MemberLoginToken")
-		if (len(loginToken) != 0 && jwt.RefreshToken("user")) || (len(memberLoginToken) != 0 && jwt.RefreshToken("member")) {
-			roles := [4]string{"Business Owner", "Team Leader", "Team Member", "Freelancer"}
-			fmt.Println("Following are the roles")
 
+		// Check if the user or member is logged in and has valid login tokens
+		if (len(loginToken) != 0 && jwt.RefreshToken("user")) || (len(memberLoginToken) != 0 && jwt.RefreshToken("member")) {
+			// Define the available roles
+			roles := [4]string{
+				"Business Owner",
+				"Team Leader",
+				"Team Member",
+				"Freelancer",
+			}
+
+			fmt.Println("Following are the roles:")
+
+			// Print the roles with index numbers
 			index := 1
 			for _, value := range roles {
 				fmt.Printf("%d. %s\n", index, value)
