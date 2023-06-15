601818c7123333e4b815209893793f75a9050618
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-06-15 09:21:14 +0500

    worked on deleting the project

--- cmd/project/droppro.go
+++ cmd/project/droppro.go
@@ -1,9 +1,13 @@
 package project
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/ibilalkayy/proctl/cmd"
+	"github.com/ibilalkayy/proctl/database/mysql"
+	"github.com/ibilalkayy/proctl/database/redis"
+	"github.com/ibilalkayy/proctl/jwt"
 	"github.com/spf13/cobra"
 )
 
@@ -12,10 +16,51 @@ var dropproCmd = &cobra.Command{
 	Use:   "droppro",
 	Short: "A brief description of your command",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("droppro called")
+		boardName, _ := cmd.Flags().GetString("board")
+		projectName, _ := cmd.Flags().GetString("project")
+
+		accountEmail := redis.GetAccountInfo("AccountEmail")
+		loginToken := redis.GetAccountInfo("LoginToken")
+
+		memberAccountEmail := redis.GetAccountInfo("MemberAccountEmail")
+		memberLoginToken := redis.GetAccountInfo("MemberLoginToken")
+
+		userProject := mysql.FindProject(accountEmail, boardName, projectName)
+		memberProject := mysql.FindProject(memberAccountEmail, boardName, projectName)
+
+		userBoard := mysql.FindBoard(accountEmail, boardName)
+		memberBoard := mysql.FindBoard(memberAccountEmail, boardName)
+
+		if len(loginToken) != 0 && jwt.RefreshToken("user") {
+			if projectName == userProject {
+				if userBoard == boardName {
+					mysql.DeleteProject(accountEmail, boardName, projectName)
+					fmt.Println("The project is successfully deleted")
+				} else {
+					fmt.Println(errors.New("The board name is wrong"))
+				}
+			} else {
+				fmt.Println(errors.New("The project by this name is not present"))
+			}
+		} else if len(memberLoginToken) != 0 && jwt.RefreshToken("member") {
+			if projectName == memberProject {
+				if memberBoard == boardName {
+					mysql.DeleteProject(accountEmail, boardName, projectName)
+					fmt.Println("The project is successfully deleted")
+				} else {
+					fmt.Println(errors.New("The board name is wrong"))
+				}
+			} else {
+				fmt.Println(errors.New("TThe project by this name is not present"))
+			}
+		} else {
+			fmt.Println(errors.New("First login to add the project"))
+		}
 	},
 }
 
 func init() {
 	cmd.RootCmd.AddCommand(dropproCmd)
+	dropproCmd.Flags().StringP("board", "b", "", "Specify the board name to delete")
+	dropproCmd.Flags().StringP("project", "p", "", "Specify the project name to delete")
 }
--- database/mysql/delete.go
+++ database/mysql/delete.go
@@ -38,3 +38,19 @@ func DeleteBoard(email, board string) {
 		fmt.Println(errors.New("Flags are required to delete the board")) // Print an error message if required values are missing
 	}
 }
+
+func DeleteProject(email, board, project string) {
+	db := Connect()                                                        // Connect to the MySQL database
+	q := "DELETE FROM Projects WHERE emails=? AND boards=? AND projects=?" // SQL query to delete a board
+	delete, err := db.Prepare(q)                                           // Prepare the SQL statement
+	middleware.HandleError(err)                                            // Handle any errors that occur
+
+	defer delete.Close() // Close the prepared statement after the function ends
+
+	if len(email) != 0 && len(board) != 0 && len(project) != 0 {
+		_, err = delete.Exec(email, board, project) // Execute the prepared statement with the provided values
+		middleware.HandleError(err)                 // Handle any errors that occur during execution
+	} else {
+		fmt.Println(errors.New("Flags are required to delete the project")) // Print an error message if required values are missing
+	}
+}
